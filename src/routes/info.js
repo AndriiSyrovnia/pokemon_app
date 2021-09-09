bc5c8e321e506ad06bc9071e440337956ce36107
Andrii Syrovnia <dalaiokes@gmail.com>
2021-09-09 18:45:49 +0300

    second commit

--- src/routes/info.js
+++ src/routes/info.js
@@ -1,14 +1,56 @@
-import React from "react";
-import FavButton  from "./../components/fav_button/FavButton";
-import Header  from "./../components/header/header";
-import Photo from "./../components/photo/photo";
-import Table from "./../components/table/table";
+import {React, Component} from "react";
+import Card from "../components/card/card";
+// import FavButton  from "./../components/fav_button/FavButton";
+// import Header  from "./../components/header/header";
+// import Photo from "./../components/photo/photo";
+// import Table from "./../components/table/table";
 
-export default function Info() {
-    return <h2>Info
-      <Header />
-      <Photo />
-      <Table />
-    <FavButton/>
-    </h2>;
-  };
+export default class Info extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=6")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.results,
+          });
+        },
+        // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+        // чтобы не перехватывать исключения из ошибок в самих компонентах.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map((item) => (
+            <li key={item.url}>
+              <Card name={item.name} url={item.url}/>
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
